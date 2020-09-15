df3924ce991e58336923b2b5543c1643c4f75122
BNanyingi <nanyingibarbara@gmail.com>
2020-09-15 21:56:18 +0300

    first commit

---
+++ barbs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 0; i<5; i++){
+    for(int j = 0; j<2; j++){
+    cout<<"a[ "<<i<<"]["<<j<<"]
+    }
+    cout<<a[i][j]<<endl;
+    }
+
+    return 0;
+}
