4cf42912f31161e93c44147cecb092c3685aacfc
Collins Koech <collinskoechck34@gmail.com>
2020-09-17 18:18:54 +0300

    update

--- Java/DrawStarShape.java
+++ Java/DrawStarShape.java
@@ -11,16 +11,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class DrawStarShape{
-    public static void main(String [] args){
-       SwingUtilities.invokeLater(new Runnable(){
+public class DrawStarShape
+{
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
             @Override
-            public void run(){
+            public void run()
+            {
                 createAndShowGUI();
             }
-       });
+        });
     }
-    private static void createAndShowGUI(){
+
+    private static void createAndShowGUI()
+    {
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.getContentPane().add(new DrawStarShapePanel());
@@ -28,10 +34,14 @@ public class DrawStarShape{
         f.setLocationRelativeTo(null);
         f.setVisible(true);
     }
+
 }
 
-class DrawStarShapePanel extends JPanel{
-    @Override protected void paintComponent(Graphics gr){
+class DrawStarShapePanel extends JPanel
+{
+    @Override
+    protected void paintComponent(Graphics gr)
+    {
         super.paintComponent(gr);
         Graphics2D g = (Graphics2D) gr;
         g.setColor(Color.WHITE);
@@ -54,41 +64,49 @@ class DrawStarShapePanel extends JPanel{
             new Point(200, 400), 50, new float[] { 0, 0.3f, 1 }, 
             new Color[] { Color.RED, Color.YELLOW, Color.ORANGE }));
         g.fill(createStar(200, 400, 40, 50, 20, 0));
+
     }
 
     private static Shape createDefaultStar(double radius, double centerX,
-        double centerY){
+        double centerY)
+    {
         return createStar(centerX, centerY, radius, radius * 2.63, 5,
             Math.toRadians(-18));
     }
 
     private static Shape createStar(double centerX, double centerY,
         double innerRadius, double outerRadius, int numRays,
-        double startAngleRad){
-            Path2D path = new Path2D.Double();
+        double startAngleRad)
+    {
+        Path2D path = new Path2D.Double();
         double deltaAngleRad = Math.PI / numRays;
-        for (int i = 0; i < numRays * 2; i++){
+        for (int i = 0; i < numRays * 2; i++)
+        {
             double angleRad = startAngleRad + i * deltaAngleRad;
             double ca = Math.cos(angleRad);
             double sa = Math.sin(angleRad);
             double relX = ca;
             double relY = sa;
-            if ((i & 1) == 0){
+            if ((i & 1) == 0)
+            {
                 relX *= outerRadius;
                 relY *= outerRadius;
-            }else{
+            }
+            else
+            {
                 relX *= innerRadius;
                 relY *= innerRadius;
             }
             if (i == 0)
             {
                 path.moveTo(centerX + relX, centerY + relY);
-            }else{
+            }
+            else
+            {
                 path.lineTo(centerX + relX, centerY + relY);
             }
         }
         path.closePath();
         return path;
-        }
-
+    }
 }
